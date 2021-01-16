3da2175a785792e094dcd0ac5ccf75edf2446c32
Astr <Astr@ASTRPC>
2021-01-17 04:00:21 +0800

    Cleanup app, Change domain

--- SoulWorkerPropertySimulator.Web/Program.cs
+++ SoulWorkerPropertySimulator.Web/Program.cs
@@ -1,12 +1,5 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SoulWorkerPropertySimulator.Data;
 
 namespace SoulWorkerPropertySimulator.Web
@@ -18,9 +11,6 @@ namespace SoulWorkerPropertySimulator.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped(
-                sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSoulWorkerPropertySimulator().InjectData();
 
             await builder.Build().RunAsync();
